29e7047c189edbe7d795bdbfe5edd852226bbcf9
dermao <15687721774@163.com>
2024-06-02 16:07:17 +0800

    day2.1

--- iweb_derxt_common/src/main/java/com/iweb/derxt/common/constants/RedisKey.java
+++ iweb_derxt_common/src/main/java/com/iweb/derxt/common/constants/RedisKey.java
@@ -2,9 +2,9 @@ package com.iweb.derxt.common.constants;
 
 public class RedisKey {
 
-    public static final String WX_STATE_KEY = "WX_STATE_KEY";
+    public static final String WX_STATE_KEY = "WX_STATE_KEY_";
     public static final String REFRESH_TOKEN = "REFRESH_TOKEN";
     public static final String GZH_REFRESH_TOKEN = "GZH_REFRESH_TOKEN";
-    public static final String TOKEN = "TOKEN";
-    public static final String LOGIN_USER_TOKEN = "LOGIN_USER_TOKEN";
+    public static final String TOKEN = "TOKEN_";
+    public static final String LOGIN_USER_TOKEN = "LOGIN_USER_TOKEN_";
 }
